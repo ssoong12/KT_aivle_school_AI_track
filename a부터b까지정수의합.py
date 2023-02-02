ff428fdfb46ac60a864abc9f2fa601cd896065fe
seunghyun lee <ssoong12@naver.com>
2023-02-02 11:48:55 +0900

    a~b 사이 정수의 합 구하기 (반복문)

---
+++ a부터b까지정수의합.py
@@ -0,0 +1,19 @@
+# 반복하는 알고리즘
+# a부터 b까지의 정수의 합 구하기 - 반복 과정에서 조건 판단하기 1
+print('a부터 b까지 정수의 합을 구합니다.')
+a = int(input('정수 a를 입력하세요. :'))
+b = int(input('정수 b를 입력하세요. :'))
+
+if a > b:
+  a, b = b, a #튜플
+
+sum = 0
+
+for i in range(a, b+1):
+  if i < b:
+    print(f' {i} +', end='')
+  else:
+    print(f'{i} = ', end='')
+  sum += i
+
+print(sum)
