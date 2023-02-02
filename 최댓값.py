fc100083e092b28c20aec507b851354369902ad5
seunghyun lee <lsh000612@gmail.com>
2023-02-02 10:19:50 +0900

    최대값 구하기

---
+++ 최댓값.py
@@ -0,0 +1,12 @@
+# 네 정수의 최댓값을 구하는 함수를 작성하고, 값을 입력받아 최댓값을 출력하라
+def max4(a, b, c, d):
+  maximum = a
+  if b > maximum:
+    maximum = b
+  if c > maximum:
+    maximum = c
+  if d > maximum:
+    maximum = d
+  return maximum
+
+print(f'max4(156, 879, 14, 475) = {max4(156, 879, 14, 475)}')
