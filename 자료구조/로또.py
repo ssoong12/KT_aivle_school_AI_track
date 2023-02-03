5eacdf45e871442385d5d9c3efb2457860050b0c
seunghyun lee <ssoong12@naver.com>
2023-02-03 16:22:19 +0900

    로또 확률

---
+++ 자료구조/로또.py
@@ -0,0 +1,8 @@
+def solution(lottos, win_nums):
+  rank_rules = {6:1, 5:2, 4:3, 3:4, 2:5, 1:6, 0:6}
+  undatemined = lottos.count(0)
+  winning = 0
+  for i in lottos:
+    if i in win_nums:
+      winning += 1
+  return [rank_rules[winning+undatemined], rank_rules[winning]]
