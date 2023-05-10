6a98e383bdd9634340553028d92bc9f330235cb7
seunghyun lee <ssoong12@naver.com>
2023-05-10 17:03:40 +0900

    GET, POST 통신 (+Query String, Body)

--- 20. Django/mysite/mysite/views.py
+++ 20. Django/mysite/mysite/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-def index(req):
-    html = """<!DOCTYPE html>
+from django.views.decorators.csrf import csrf_exempt
+
+html = """<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
@@ -11,22 +12,39 @@ def index(req):
   <body>
     <h1><a href="/">Django</a></h1>
     <ol>
-      <li><a href="/chapter01">Setting & Deploy</a></li>
-      <li><a href="/chapter02">Routing & View</a></li>
+      <li><a href="/chapter/01/">Setting & Deploy</a></li>
+      <li><a href="/chapter/02/">Routing & View</a></li>
     </ol>
-    <h2>Django</h2>
+    <h2>{title}</h2>
+    <p>{content}</p>
     <p><a href="https://www.djangoproject.com/" target="_blank">Django</a>는 Python으로 작성된 오픈 소스 웹 프레임워크로, 빠르고 쉬운 웹 개발을 가능하게 합니다.</p>
   </body>
 </html>"""
-    return HttpResponse(html)
-  
-def list(req):
-  return HttpResponse("List")
 
-def post(req, id):
-  return HttpResponse(f"post {id}이다!")
+chapters = {
+  "01" : {
+    "title" : "Setting & Deploy",
+    "content" : "Setting & Deploy is .... 어쩌구저쩌궁",
+  },
+  "02" : {
+    "title" : "Routing & View",
+    "content" : "Routing & View is .... 어쩌구저쩌궁",
+  }
+}
 
-def gugu(req, num):
-  num = int(num)
-  gugudan = [f"{num} * {i} = {num * i}" for i in range(1, 21)]
-  return HttpResponse(f"<br>".join(gugudan))
+def index(req):
+  d = {
+    'title' : 'django',
+    'content' : 'django is 어쩌구저꺼웅... 인덱스임'
+  }
+  return HttpResponse(html.format(**d))
+
+def chapter(req, id):
+  return HttpResponse(html.format(**chapters.get(id)))
+
+@csrf_exempt
+def search(request):
+  print(request.method)
+  print(f"Query String: {request.GET.get('q')}")
+  print(f"BODY: {request.POST.get('key', '')}")
+  return HttpResponse(f'search')
