fea1ac70268f80812d03107730b88e6ae38a7d18
seunghyun lee <ssoong12@naver.com>
2023-05-12 13:58:27 +0900

    관리자 사이트에 모델 추가해서 데이터 관리

--- 20. Django/day3/blog/admin.py
+++ 20. Django/day3/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+# 모델 추가
+from . import models
+
+admin.site.register(models.Post)
--- 20. Django/day3/blog/models.py
+++ 20. Django/day3/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# Post 모델 추가
+class Post(models.Model):
+  title = models.CharField(max_length=100)
+  content = models.TextField()
+  created_at = models.DateField(auto_now_add=True)
+  published_at = models.DateField(null=True)
