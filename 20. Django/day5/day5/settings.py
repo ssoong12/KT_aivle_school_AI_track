938cfeed87382c7001559926f0e09c6cb571a90a
seunghyun lee <ssoong12@naver.com>
2023-05-16 12:32:47 +0900

    document 사용법(swagger, redoc과의 쉬운 연계) - API 명세화

--- 20. Django/day5/day5/settings.py
+++ 20. Django/day5/day5/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django_extensions",
     "rest_framework",
+    "drf_yasg",
     "blog",
 ]
 
--- 20. Django/day5/day5/urls.py
+++ 20. Django/day5/day5/urls.py
@@ -15,10 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from . import views
+
+# documentation, api를 사용하는 사람들을 위해 명세화 해준다
+schema_view = get_schema_view(
+   openapi.Info(
+      title="KT Django API v1.0", default_version='v1',
+      contact=openapi.Contact(email="blackdew7@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -29,6 +41,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('blog/', include("blog.urls")),
     path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     
 ]
